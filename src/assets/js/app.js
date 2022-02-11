7415d7a4aae747d81a319c7870c1a18d5ee1863e
frontdevruso <frontdevruso@gmail.com>
2022-02-11 18:42:11 +0300

    10 lines

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,7 +1,14 @@
-const reviewItem = document.querySelectorAll('.reviews-slider__item-info p');
+const reviewItems = document.querySelectorAll('.reviews-slider__item-info p');
+const mainSliderItems = document.querySelectorAll('.main-service__desc');
 
-if (reviewItem) {
-    reviewItem.forEach(function(item) {
+if (reviewItems) {
+    reviewItems.forEach(function(item) {
+        item.style.setProperty("-webkit-box-orient", "vertical");
+    })
+}
+
+if (mainSliderItems) {
+    mainSliderItems.forEach(function(item) {
         item.style.setProperty("-webkit-box-orient", "vertical");
     })
 }
