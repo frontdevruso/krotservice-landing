9f2d617f045cb3c852554ad6479ba863ffa0fc51
frontdevruso <frontdevruso@gmail.com>
2022-02-18 08:42:36 +0300

    work slider ref

--- src/assets/js/slider-works.js
+++ src/assets/js/slider-works.js
@@ -1,10 +1,11 @@
-import Swiper, { Navigation } from "swiper";
+import Swiper, { Navigation, Autoplay } from "swiper";
 
 new Swiper(".works-slider", {
-    modules: [Navigation],
-    slidesPerView: 1,
+    modules: [Navigation, Autoplay],
+    slidesPerView: "auto",
     spaceBetween: 30,
     slidesPerGroup: 1,
+    centeredSlides: false,
     
     grid: {
         rows: 1,
@@ -15,11 +16,32 @@ new Swiper(".works-slider", {
         nextEl: ".works-slider__btn--next",
     },
 
+    autoplay: {
+        delay: 700,
+    },
+
     breakpoints: {
+        1230: {
+            centeredSlides: false,
+            slidesPerView: 3,
+        },
+        768: {
+            centeredSlides: false,
+            slidesPerView: 2,
+        },
         500: {
+            centeredSlides: true,
             slidesPerView: "auto",
+        },
+        1: {
+            centeredSlides: true,
+            slidesPerView: 1,
             spaceBetween: 30,
             slidesPerGroup: 1,
+
+            autoplay: {
+                delay: 1000,
+            },
             
             grid: {
                 rows: 1,
